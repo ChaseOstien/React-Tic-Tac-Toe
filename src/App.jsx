377bde9fe7cef2c3425059d44b4dfaa86c620402
ChaseOstien <129995308+ChaseOstien@users.noreply.github.com>
2023-12-20 19:40:59 -0600

    Created game history, new parent component game. Lifted state up to parent component game

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Square from './components/Square';
+import Board from './components/Board'
 
-export default function Board() {
+export default function Game() {
+    const [xIsNext, setXIsNext] = useState(true);
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const currentSquares = history[history.length - 1];
 
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
-    function handleClick(i) {
-        const nextSquares = squares.slice();
-        nextSquares[i] = 'X';
-        setSquares(nextSquares);
+    function handlePlay(nextSquares) {
+      // TODO
+      setHistory([...history, nextSquares]);
+      setXIsNext(!xIsNext);
     }
 
-  return ( 
-    <>
-      <div className='board-row'>
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
       </div>
-      <div className='board-row'>
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      <div className='game-info'>
+        <ol>
+          {/*ToDo*/}
+        </ol>
       </div>
-      <div className='board-row'>
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
-    </>
-  );
+    </div>
+  )
 }
+
+
